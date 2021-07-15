20e196996e5935bcedf5527771313893229d9e09
vichaw <82095305+vichaw@users.noreply.github.com>
2021-07-15 21:25:06 +0530

    Update AM Bullet Chart.js

--- AM Bullet Chart.js
+++ AM Bullet Chart.js
@@ -1,5 +1,5 @@
 (function() {
-	
+	/*
 	let src1="https://cdn.amcharts.com/lib/4/core.js";
 	let src2="https://cdn.amcharts.com/lib/4/charts.js";
 	let src3="https://cdn.amcharts.com/lib/4/themes/animated.js";
@@ -15,15 +15,10 @@
 	script1.src = src1;
 	script2.src = src2;
 	script3.src = src3; 
-	/*
+	
 	document.getElementsByTagName('head')[0].appendChild(script1);
 	document.getElementsByTagName('head')[0].appendChild(script2);
 	document.getElementsByTagName('head')[0].appendChild(script3); */
-	
-	this.appendChild(script1);
-	this.appendChild(script2);
-	this.appendChild(script3); 
-	
 	
 	let template = document.createElement("template");
     template.innerHTML = `
@@ -33,7 +28,27 @@
     class Amchart extends HTMLElement {
         constructor() {
             super();
+		
+		let src1="https://cdn.amcharts.com/lib/4/core.js";
+		let src2="https://cdn.amcharts.com/lib/4/charts.js";
+		let src3="https://cdn.amcharts.com/lib/4/themes/animated.js";
 
+		let script1 = document.createElement('script');
+		let script2 = document.createElement('script');
+		let script3 = document.createElement('script');
+
+		script1.type = 'text/javascript';
+		script2.type = 'text/javascript';
+		script3.type = 'text/javascript';
+
+		script1.src = src1;
+		script2.src = src2;
+		script3.src = src3; 
+		
+		this.appendChild(script1);
+		this.appendChild(script2);
+		this.appendChild(script3); 
+		
             let shadowRoot = this.attachShadow({
                 mode: "open"
             });
